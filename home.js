a3cf956139ce0ac96f068904463320ab3aae5af6
heba <hebe10@yahoo.com>
2020-03-10 20:55:14 +0200

    add style to dom elemant native javascript only

--- home.js
+++ home.js
@@ -8,17 +8,18 @@ var j='3';
 function getData() {
     
     
-    document.write('<ul>\
-    <li>\
-      <a href="home.html">Home</a>\
+    document.write('<ul style="list-style-type: none;margin: 0;padding: 0;overflow: hidden;background-color: #333;">\
+    <li style="float: left;">\
+      <a href="home.html" style="display: block;color: white;text-align: center;padding: 14px 16px; text-decoration: none;" >Home</a>\
     </li>\
-    <li>\
-      <a href="#">About</a>\
+    <li style="float: left;">\
+      <a href="#" style="display: block;color: white;text-align: center;padding: 14px 16px; text-decoration: none;">About</a>\
     </li>\
-    <li>\
-      <a href="#">Contact us</a>\
+    <li style="float: left;">\
+      <a href="#" style="display: block;color: white;text-align: center;padding: 14px 16px; text-decoration: none;">Contact us</a>\
     </li>\
     </ul>');
+
    
 const xhr  = new XMLHttpRequest();
  
@@ -33,14 +34,37 @@ xhr .onload = ()=>
    
 for(let i=0;i<res.length;i++)
     {
+      
         let divv = document.createElement("div");
+       
+       
+        
+
+        let name = document.createElement("p");
         let imgg = document.createElement("img");
-        let id = document.createElement("p");
+        let price = document.createElement("p");
+
+        name.textContent =(res[i].Name);
         imgg.setAttribute("src",res[i].ProductPicUrl);
-        id.textContent =(res[i].ProductId);
+        price.textContent =(res[i].Price);
+
+
         document.body.appendChild(divv);
+        
+        divv.appendChild(name);
         divv.appendChild(imgg);
-        divv.appendChild(id);
+        divv.appendChild(price);
+        //////////////////////////
+        price.style.color = "red";
+        price.style.font = "italic bold 20px arial,serif";
+        price.style.fontSize = "xx-large";
+        
+        //////////////////////
+        divv.style.textAlign = "center";
+        divv.style.outline = "thick solid #0000FF";
+
+        
+       
 }
 
 
@@ -49,6 +73,7 @@ for(let i=0;i<res.length;i++)
 let minus= document.createElement("button");
 minus.textContent="<<<";
 document.body.appendChild(minus);
+
 minus.addEventListener('click',(ev) =>
          {  
           
@@ -60,18 +85,101 @@ minus.addEventListener('click',(ev) =>
         
          
 
+});
+///////////////////////////////////
+let one= document.createElement("button");
+one.textContent="1";
+document.body.appendChild(one);
+one.addEventListener('click',(ev) =>
+         {  
+          
+       document.body.innerHTML="";   
+     
+        
+         p=1;
+         
+         getData();
+        
+         
+
+
+});
+///////////////////////////////////
+let two= document.createElement("button");
+two.textContent="2";
+document.body.appendChild(two);
+two.addEventListener('click',(ev) =>
+         {  
+          
+       document.body.innerHTML="";   
+     
+        
+         p=2;
+         getData();
+        
+         
+
+
+});
+///////////////////////////////////
+let three= document.createElement("button");
+three.textContent="3";
+document.body.appendChild(three);
+three.addEventListener('click',(ev) =>
+         {  
+          
+       document.body.innerHTML="";   
+     
+        
+         p=3;
+         getData();
+        
+         
+
+
+});
+///////////////////////////////////
+let four= document.createElement("button");
+four.textContent="4";
+document.body.appendChild(four);
+four.addEventListener('click',(ev) =>
+         {  
+          
+       document.body.innerHTML="";   
+     
+        
+         p=4;
+         getData();
+        
+         
+
+
+});
+///////////////////////////////////
+let five= document.createElement("button");
+five.textContent="5";
+document.body.appendChild(five);
+five.addEventListener('click',(ev) =>
+         {  
+          
+       document.body.innerHTML="";   
+     
+        
+         p=5;
+         getData();
+        
+         
+
 
 });
 
-
-///////////////////////
 let plus= document.createElement("button");
 plus.textContent=">>>";
 document.body.appendChild(plus);
 plus.addEventListener('click',(ev) =>
          {  
         document.body.innerHTML="";
-        // document.getElementById('header').innerHTML="";
+        
         
         
 
@@ -83,9 +191,13 @@ plus.addEventListener('click',(ev) =>
 
 
 });
-}
 
 }
+
+
+}
+
+
 getData();
 
 
